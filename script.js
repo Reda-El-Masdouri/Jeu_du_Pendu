bcee64fd4ff579f6968bec670e5520bca2e2a0a9
Reda E <reda.elmasdouri@gmail.com>
2022-12-20 22:50:19 +0100

    themes.js est le fichier qui contient une base de données des themes du jeu. ajout de la table des themes (jquery) aussi l'evenement onclick sur chaque theme. le keyListener commence quand l'utilisateur choisi un theme. quand l'utilisateur change de theme le jeu repart à zero. un style de la table des themes est ajouté.

--- script.js
+++ script.js
@@ -4,18 +4,57 @@ const btnRejouer = $('#btn-rejouer');
 const messageFinal = $('#message-final-container');
 const notification = $('#notification-container');
 const message = $('#message');
-
+const themesTable = $('.all-themes');
 const bonhommeParts = $('.bonhomme-part');
 
+const themesJSON = [ecoleJSON, paysagesClimatJSON, calculMesuresJSON, alimentsJSON, gestesMouvementsJSON];
+const themes = themesJSON.map(JSON.parse);
 
-const mots = ['ordinateur', 'programmation', 'javascript', 'html', 'universite'];
+// remplir la table des themes
+themesTable.html(`
+    ${themes.map(oneTheme => `<tr><td class="oneTheme">${oneTheme.intitule}</td></tr>`)}
+    `);
 
-let motSelectionne = mots[Math.floor(Math.random() * mots.length)];
-
-console.log(motSelectionne);
+//const ecoleTheme = JSON.parse(ecoleJSON);
+//console.log(themes);
+//console.log(ecoleJSON);
+//console.log(ecoleTheme.theme);
+//const mots = ['ordinateur', 'programmation', 'javascript', 'html', 'universite'];
+//let motSelectionne = mots[Math.floor(Math.random() * mots.length)];
+//let selection = ecoleTheme.theme[Math.floor(Math.random() * ecoleTheme.theme.length)];
+//let motSelectionne = selection.mot;
+//let description = selection.description;
 
 const tabBonneLettres = [];
 const tabMauvaisesLettres = [];
+
+var selection;
+var themeSelectione;
+var motSelectionne;
+var description;
+
+function addClickOnTd() {
+    jQuery.each($('.oneTheme'), (index, maClasse) => {
+        $(maClasse).on('click', function() {
+            
+            tabBonneLettres.splice(0);
+            tabMauvaisesLettres.splice(0);
+            
+            themeSelectione = themes[index];
+            selection = themeSelectione.theme[Math.floor(Math.random() * themeSelectione.theme.length)];
+            motSelectionne = selection.mot;
+            description = selection.description;
+            console.log(motSelectionne);
+            
+            updateMauvaisesLettres();
+            
+            afficherMot();
+            
+        })
+    });
+}
+
+
 
 // fonction pour afficher le motSelectionne:
 function afficherMot() {
@@ -27,17 +66,22 @@ function afficherMot() {
             ).join('')}`
     );
     //console.log(mot.text());
+    $('#description').text(description)
     if(mot.text() === motSelectionne){
         message.text("Bravo ! Vous avez gagné !");
         messageFinal.css({"display":"flex"});
+        $(window).off('keydown');
     }
 }
 
 // update mauvaises lettres:
 function updateMauvaisesLettres() {
     // affichage des mauvaises lettres
-    if(tabMauvaisesLettres.length > 0)
+    if(tabMauvaisesLettres.length != 0){
         $('.mauvaises-lettres-container p').text("Mauvaises lettres");
+    } else{
+        $('.mauvaises-lettres-container p').text("");
+    }
     mauvaisesLettres.html(`
         ${tabMauvaisesLettres.map(lettre => `<span>${lettre}</span>`)}
     `);
@@ -54,6 +98,7 @@ function updateMauvaisesLettres() {
     if(tabMauvaisesLettres.length === bonhommeParts.length){
         message.text("Vous avez perdu !");
         messageFinal.css({"display":"flex"});
+        $(window).off('keydown');
     }
 }
 
@@ -66,35 +111,49 @@ function afficherNotification() {
 }
 
 // eventListener
-$(window).keydown(e => {
-    // les keyCode des lettres varient entre 65=>a et 90=>z. keyCod = 65 et key = a.
-    if(e.keyCode >= 65 && e.keyCode <= 90){
-        const lettre = e.key;
-        if(motSelectionne.includes(lettre)){
-            if(!tabBonneLettres.includes(lettre)){
-                tabBonneLettres.push(lettre);
-                afficherMot();
-            } else {
-                afficherNotification();
-            }
-        } else {
-            if(!tabMauvaisesLettres.includes(lettre)){
-                tabMauvaisesLettres.push(lettre);
-                updateMauvaisesLettres();
-            } else {
-                afficherNotification();
+function activateKeys() {
+    $(window).keydown(e => {
+        if(typeof motSelectionne !== "undefined"){
+            // les keyCode des lettres varient entre 65=>a et 90=>z. keyCod = 65 et key = a.
+            if(e.keyCode >= 65 && e.keyCode <= 90){
+                const lettre = e.key;
+                if(motSelectionne.includes(lettre)){
+                    if(!tabBonneLettres.includes(lettre)){
+                        tabBonneLettres.push(lettre);
+                        afficherMot();
+                    } else {
+                        afficherNotification();
+                    }
+                } else {
+                    if(!tabMauvaisesLettres.includes(lettre)){
+                        tabMauvaisesLettres.push(lettre);
+                        updateMauvaisesLettres();
+                    } else {
+                        afficherNotification();
+                    }
+                }
             }
         }
-    }
-});
+    });
+}
 
 btnRejouer.click(e => {
     tabBonneLettres.splice(0);
     tabMauvaisesLettres.splice(0);
-    motSelectionne = mots[Math.floor(Math.random() * mots.length)];
+
+    selection = themeSelectione.theme[Math.floor(Math.random() * themeSelectione.theme.length)];
+    motSelectionne = selection.mot;
+    description = selection.description;
+
+    $(window).on('keydown', activateKeys());
     updateMauvaisesLettres();
     afficherMot();
     messageFinal.css({"display":"none"});
 })
 
-afficherMot();
+function startGame() {
+    addClickOnTd();
+    activateKeys();
+}
+
+startGame();
